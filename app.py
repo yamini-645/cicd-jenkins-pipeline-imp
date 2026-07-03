cf60ef35b062f5c2ba1e7400b595f138971da208
Yamini Nelluru <yamini.nelluru86@gmail.com>
2026-07-03 13:34:57 +0000

    Updated application landing page

--- app.py
+++ app.py
@@ -4,7 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps CI/CD Pipeline!"
+    return """
+    <h1>🚀 CI/CD Pipeline Automation using Jenkins</h1>
+
+    <p><b>🟢 Application Status:</b> Running Successfully</p>
+
+    <p>
+    This project demonstrates an end-to-end CI/CD pipeline that automatically builds, pushes, and deploys a Dockerized Flask application using Jenkins Pipeline.
+    </p>
+
+    <h3>Pipeline Highlights</h3>
+
+    <ul>
+        <li>✅ Source Code: GitHub</li>
+        <li>✅ CI Tool: Jenkins Pipeline</li>
+        <li>✅ Containerization: Docker</li>
+        <li>✅ Image Registry: Docker Hub</li>
+        <li>✅ Deployment: Automated using <code>deploy.sh</code></li>
+    </ul>
+
+    <p><b>📦 Version:</b> v2.0</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
